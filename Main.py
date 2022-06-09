a8d7e3bc6830f2f519a7af37f817ff75d5d5edda
jrang <98jugo@gmail.com>
2022-06-09 15:08:53 +0200

    Add main.py

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import requests
 from super_secret_info import *
 from itertools import chain
+from datetime import datetime
 
 
 def get_orders_active(venue):
@@ -16,13 +17,15 @@ def get_trades(venue):
     response = requests.get(f'{BASE}/{venue}/api/trades', headers={'API-Key': API_KEY})
     response.raise_for_status()
     to_return = response.json()['data']
+    format = "%d %m %Y %H:%M:%S.%f"
+    datetime.strptime(ts, format)
     for item in to_return:
         item['price'] = float(item['price'])
+        item['timestamp']= datetime.strptime(ts, format)
     return to_return
 
 print(get_trades("bluelagoon"))
 data = get_trades("bluelagoon")
-ts = '09 06 2022 14:51:54.914235'
 
-from datetime import datetime
-datetime.strptime(ts, ...)
+
+

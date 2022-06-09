9c5e6ecf720e9972b981066f085f7e5ae6f3c30b
Thom <tpieterman93@gmail.com>
2022-06-09 18:14:37 +0200

    removed api_orderbook.py

--- api_orderbook.py
+++
@@ -1,9 +0,0 @@
-import requests
-from super_secret_info import *
-
-def get_orderbook(venue):
-    response = requests.get(f'{BASE}/{venue}/api/orderbook', headers={'API-Key': API_KEY})
-    response.raise_for_status()
-    return response.json()
-
-print(get_orderbook('bluelagoon'))

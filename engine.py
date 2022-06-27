2f1d0b8850219e0cc146765ba0fd06aae5128767
Rik <rik.de.kort@hey.com>
2022-06-27 21:22:23 +0200

    Rely on sign of amount to indicate buy or sell, and unwrap dictionaries in functions

--- engine.py
+++ engine.py
@@ -31,7 +31,7 @@ def make_orderbook():
     c = book.cursor()
     c.execute(
         # Timestamp is logical timestamp
-        'create table exchange (participant integer, price integer, amount integer, timestamp integer primary key autoincrement, is_buy bool)')
+        'create table exchange (participant integer, price integer, amount integer, timestamp integer primary key autoincrement)')
     c.execute('create table accounts (participant integer, balance integer)')
     c.close()
     return book
@@ -44,11 +44,20 @@ def orderbook():
     book.close()
 
 
-def insert_order(book: sqlite3.Cursor, order: dict):
+def insert_order(book: sqlite3.Cursor, participant: str, price: int, amount: int):
     book.execute(
-        'insert into exchange(participant, price, amount, is_buy) values(:participant, :price, :amount, :is_buy)',
-        order)
+        'insert into exchange(participant, price, amount) values(:participant, :price, :amount)',
+        {'participant': participant, 'price': price, 'amount': amount})
     return book.lastrowid
+
+
+def test_insert_order(orderbook):
+    order = {'participant': 0, 'price': 31, 'amount': 5}
+    c = orderbook.cursor()
+    insert_order(c, **order)
+    book, accounts = read(c)
+    c.close()
+    assert book == OrderFree([order], skip=['timestamp'])
 
 
 def insert_accounts(book: sqlite3.Cursor, accounts: list[dict]):
@@ -56,54 +65,54 @@ def insert_accounts(book: sqlite3.Cursor, accounts: list[dict]):
 
 
 def read(book: sqlite3.Cursor) -> tuple[list[dict], list[dict]]:
-    cols = ('participant', 'price', 'amount', 'timestamp', 'is_buy')
+    cols = ('participant', 'price', 'amount', 'timestamp')
     exchange = [dict(zip(cols, row)) for row in book.execute('select * from exchange').fetchall()]
     cols = ('participant', 'balance')
     accounts = [dict(zip(cols, row)) for row in book.execute('select * from accounts').fetchall()]
     return exchange, accounts
 
 
-def limit_order(c: sqlite3.Cursor, order: dict):
+def limit_order(c: sqlite3.Cursor, *, participant: str, price: int, counter_amount: int):
     # Insert transaction into order book so it gets a timestamp
-    order['timestamp'] = insert_order(c, order)
+    timestamp = insert_order(c, participant=participant, price=price, amount=counter_amount)
 
     # Fetch matching transactions from the order c
     matching = c.execute(
-        'select participant, timestamp, amount, price from exchange where not is_buy and price <= ? order by price desc, timestamp asc',
-        (order['price'],)
+        'select participant, timestamp, amount, price from exchange where amount < 0 and price <= ? order by price desc, timestamp asc',
+        (price,)
     ).fetchall()
 
     # Fulfill transactions in turn
-    remaining = order['amount']
+    remaining = counter_amount
     delta = defaultdict(lambda: 0)
     fulfilled = []
-    for idx, ts, amount, price in matching:
-        if remaining > amount:
+    for idx, ts, counter_amount, price in matching:
+        if remaining > -counter_amount:
             # "Eat" this order, still some appetite left
-            remaining -= amount
+            remaining += counter_amount
 
-            delta[idx] += amount * price
-            delta[order['participant']] -= amount * price
+            delta[idx] -= counter_amount * price
+            delta[participant] += counter_amount * price
 
             fulfilled.append(ts)
-        elif remaining == amount:
+        elif remaining == -counter_amount:
             # "Eat" this order, appetite fulfilled
-            delta[idx] += amount * price
-            delta[order['participant']] -= amount * price
+            delta[idx] -= counter_amount * price
+            delta[participant] += counter_amount * price
 
             fulfilled.append(ts)
-            fulfilled.append(order['timestamp'])
+            fulfilled.append(timestamp)
             break
-        elif remaining < amount:
+        elif remaining < -counter_amount:
             # Appetite fulfilled, but this order too big
-            c.execute('update exchange set amount=? where timestamp=?', (amount - remaining, ts))
-            fulfilled.append(order['timestamp'])
+            c.execute('update exchange set amount=? where timestamp=?', (-counter_amount - remaining, ts))
+            fulfilled.append(timestamp)
             break
         else:
             raise Exception('Unexpected logic error')
     else:
         # We fell off the end there: this means our order did not get completely fulfilled
-        c.execute('update exchange set amount=? where timestamp=?', (remaining, order['timestamp']))
+        c.execute('update exchange set amount=? where timestamp=?', (remaining, timestamp))
     c.executemany('delete from exchange where timestamp=?', [(ts,) for ts in fulfilled])
 
     # Update account balances
@@ -112,27 +121,27 @@ def limit_order(c: sqlite3.Cursor, order: dict):
 
 def test_no_counterparty(orderbook):
     orders = [
-        {'participant': 0, 'price': 31, 'amount': 5, 'is_buy': True},
-        {'participant': 1, 'price': 31, 'amount': 5, 'is_buy': True},
+        {'participant': 0, 'price': 31, 'amount': 5},
+        {'participant': 1, 'price': 31, 'amount': 5},
     ]
     c = orderbook.cursor()
-    limit_order(c, orders[0])
-    limit_order(c, orders[1])
+    limit_order(c, **orders[0])
+    limit_order(c, **orders[1])
     result, _ = read(c)
     assert result == OrderFree([orders[0], orders[1]], skip=['timestamp'])
 
 
 def test_buy_order(orderbook):
     orders = [
-        {'participant': 0, 'price': 31, 'amount': 5, 'is_buy': False},
-        {'participant': 1, 'price': 31, 'amount': 5, 'is_buy': True},
+        {'participant': 0, 'price': 31, 'amount': -5},
+        {'participant': 1, 'price': 31, 'amount': 5},
     ]
     accounts = [{'participant': 0, 'balance': 100}, {'participant': 1, 'balance': 100}]
 
     c = orderbook.cursor()
     insert_accounts(orderbook, accounts)
 
-    orders[0]['timestamp'] = insert_order(c, orders[0])
+    orders[0]['timestamp'] = insert_order(c, **orders[0])
     book_, accounts_ = read(c)
     assert book_ == [orders[0]] and accounts == OrderFree(accounts_)
 

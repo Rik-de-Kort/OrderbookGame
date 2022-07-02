01726efd67ff40c0d623dd21e784e34cdcd412f2
Rik <rik.de.kort@hey.com>
2022-07-02 15:47:39 +0200

    Add routes for getting balance, active orders, and orderbook

--- trading_engine/api.py
+++ trading_engine/api.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI, Depends
 
-from auth import oauth2_scheme, User, get_user_for
+from auth import User, get_user_for_token
 from auth import create_authenticated_token
+from db_utils import db_cursor, query
 
 import os
 
@@ -11,12 +12,32 @@ app = FastAPI()
 
 
 @app.get('/')
-def main(token: str = Depends(oauth2_scheme)):
-    return {'token': token}
+def home():
+    return 'Welcome to the orderbook game!'
+
+
+@app.get('/orderbook/')
+def orderbook(c=Depends(db_cursor)):
+    orderbook = query(c, 'select * from exchange')
+    return {'data': {
+        'buy': [r for r in orderbook if r['price'] >= 0],
+        'sell': [r for r in orderbook if r['price'] < 0],
+    }}
+
+
+@app.get('/balance/')
+def balance(c=Depends(db_cursor), user=Depends(get_user_for_token)):
+    balance = query(c, 'select balance from accounts natural join auth where auth.name=?', (user.name,))
+    return balance[0]
+
+
+@app.get('/orders/active')
+def balance(c=Depends(db_cursor), user=Depends(get_user_for_token)):
+    return query(c, 'select * from exchange where exchange.participant_id=?', (user.participant_id,))
 
 
 @app.get('/me/')
-def me(user: User = Depends(get_user_for)):
+def me(user: User = Depends(get_user_for_token)):
     return user
 
 
